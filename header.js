884eac9cb0f6be80e3c75f6104fa9e33cb6ef622
Nathan Gillespie <crookedtwig4@gmail.com>
2024-05-17 09:38:44 -0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,9 @@
+function createHeader() {
+    var header = document.getElementById(`header`);
+    header.innerHTML = `
+    <center>
+        <h1>Who Want's to be a Mathilionair?</h1>
+        <p>Brought to you by the "petty" foundation</p>
+    </center>
+    `;
+}
