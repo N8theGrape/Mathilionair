6a337e451d045ffb8657e4842b4eec5198358b66
Nathan Gillespie <crookedtwig4@gmail.com>
2024-05-20 15:34:21 -0700

    tx

--- header.js
+++
@@ -1,9 +0,0 @@
-function createHeader() {
-    var header = document.getElementById(`header`);
-    header.innerHTML = `
-    <center>
-        <h1>Who Want's to be a Mathilionair?</h1>
-        <p>Brought to you by the "petty" foundation</p>
-    </center>
-    `;
-}
