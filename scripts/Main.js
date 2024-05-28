a5ed8c2f49d80a03c1a262c3508a78edb1a4b11a
Nathan Gillespie <crookedtwig4@gmail.com>
2024-05-28 11:27:23 -0700

    Working Outline

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,14 +1,73 @@
-var currectQuestion = 0;
+var currentQuestion = 1;
+var selectedAnswer = 0;
+
+function loadQuestion(number) {
+    var currentQuestionData = questionData[number - 1];
+    for(var i = 1; i <= 4; i++) {
+        document.getElementById(`choice${i}`).textContent = `${i}) ${currentQuestionData.answer[i - 1]}`;
+    }
+    document.getElementById(`prompt`).textContent = `${currentQuestionData.question}`;
+}
 
 function beginGame() {
     document.getElementById("TitlePage").style.display = "none";
     document.getElementById("Game").style.display = "block";
+    loadQuestion(currentQuestion);
 }
 
 function selectChoice(choice) {
-    
+    for(var i = 1; i <= 4; i++) {
+        document.getElementById(`choice${i}`).style.backgroundColor = "#0000ff"; // Set to default color
+    }
+    document.getElementById(`choice${choice}`).style.backgroundColor = "#2244ff";
+    document.getElementById(`SubmitButton`).disabled = false;
+    selectedAnswer = choice;
 }
 
-var questions =[
-    
-];
+function submitAnswer() {
+    for(var i = 1; i <= 4; i++) {
+        document.getElementById(`choice${i}`).disabled = "true";
+    }
+    document.getElementById(`choice${selectedAnswer}`).style.backgroundColor = "gold" // Set to color for wrong answer
+    document.getElementById(`choice${questionData[currentQuestion - 1].correctAnswer}`).style.backgroundColor = "green" // Set to color for wrong answer
+    document.getElementById(`SubmitButton`).style.display = "none";
+    document.getElementById(`NextButton`).style.display = "inline";
+    if(questionData[currentQuestion - 1].correctAnswer != selectedAnswer) {
+        document.getElementById(`NextButton`).innerHTML = "Game Over<br>(Click to reload))";
+    }
+}
+
+function gameOver() {
+    document.getElementById('Game').style.display = "none";
+    document.getElementById('GameOver').style.display = "inline";
+}
+
+function resetQuestion() {
+    for(var i = 1; i <= 4; i++) {
+        document.getElementById(`choice${i}`).style.backgroundColor = "#0000ff"; // Set to default color
+        document.getElementById(`choice${i}`).disabled = false;
+    }
+    selectedAnswer = 0;
+    document.getElementById(`SubmitButton`).disabled = true;
+    document.getElementById(`SubmitButton`).style.display = "inline";
+    document.getElementById(`NextButton`).style.display = "none";
+}
+
+function nextQuestion() {
+    if(questionData[currentQuestion - 1].correctAnswer == selectedAnswer) {
+        if(currentQuestion < questionData.length) {
+            resetQuestion();
+            currentQuestion -=- 1;
+            loadQuestion(currentQuestion);
+        } else {
+            winGame();
+        }
+    } else {
+        location.reload();
+    }
+}
+
+function winGame() {
+    document.getElementById('Game').style.display = "none";
+    document.getElementById('WinScreen').style.display = "inline";
+}
---
+++ scripts/questions.js
@@ -0,0 +1,42 @@
+let questionData = [
+    {
+        question: "What is Hanafi's favorite color?",
+        answer: [
+            "Red",
+            "Green",
+            "Blue",
+            "Purple"
+        ],
+        correctAnswer: 4
+    },
+    {
+        question: "What are the requirements for the Mean Value Theorem?",
+        answer: [
+            "Continuous",
+            "Differentiable",
+            "Continuous & Differentiable",
+            "None of the Above"
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "No Question?",
+        answer: [
+            "Wrong",
+            "Wrong",
+            "Correct",
+            "Wrong"
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "What was the answer to question 1?",
+        answer: [
+            "C",
+            "A",
+            "D",
+            "B"
+        ],
+        correctAnswer: 3
+    }
+];
